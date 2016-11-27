e7b787b12c076c09791156422ca8c934e7517d05
Gideon Potok <gpotok@terpmail.umd.edu>
2016-11-27 12:43:19 -0500

    this project is ready for junit testing

--- src/testing/Test.java
+++ src/testing/Test.java
@@ -3,6 +3,9 @@ package testing;
 /**
  * Created by gideonpotok on 11/26/16.
  */
-
-public class Test {
+public class Test  {
+    @org.junit.Test
+    public void test1(){
+        org.junit.Assert.assertNull("sorry was null", null);
+    }
 }
