cad836b069e309b0f02d70837a317a6e6514a111
Marc <mslarsen1992@gmail.com>
2016-11-30 11:50:55 -0500

    Added java doc to the file.

--- src/com/company/BoardPiece.java
+++ src/com/company/BoardPiece.java
@@ -1,7 +1,16 @@
 package com.company;
 
 /**
- * Created by gideonpotok on 11/5/16.
+ * @Author Anu Challa (achalla@terpmail.umd.edu)
+ * @Author Gideon Potok (gideon.potok@gmail.com)
+ * @Author Marc Storm Larsen (mslarsen1992@gmail.com)
+ *
+ * I pledge on my honor that I have not given or received any unauthorized assistance on this project.
+ */
+
+/**
+ * The PlayerType enum is allowing for the code to be more readable.
+ * Two types of players; HUMAN and COMPUTER.
  */
 public enum BoardPiece {
     EMPTY, GOLD, BLUE
